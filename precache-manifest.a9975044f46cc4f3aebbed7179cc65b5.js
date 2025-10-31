4243d652619e5f41245f1115f7cf5878cffe756f
mmarcinowski <mateusz.marcinowski@mp.pl>
2025-10-31 10:20:31 +0100

    all

---
+++ precache-manifest.a9975044f46cc4f3aebbed7179cc65b5.js
@@ -0,0 +1,458 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41291c42d3cefe9d1d790dd3efb8f729",
+    "url": "/audio/en.mp3"
+  },
+  {
+    "revision": "d384e80902bbbb6b6e6603420580b2ea",
+    "url": "/audio/hu.mp3"
+  },
+  {
+    "revision": "f145d77120692ca8347e71a684380205",
+    "url": "/audio/pl.mp3"
+  },
+  {
+    "revision": "88774e88c571fd412f337c073a54fc09",
+    "url": "/catalog/Berki.pdf"
+  },
+  {
+    "revision": "1941edc22266e57e6a05fd561a0ef86c",
+    "url": "/catalog/Nikifor.pdf"
+  },
+  {
+    "revision": "e4f5881ebfc26d2731e2",
+    "url": "/css/app.f15ac949.css"
+  },
+  {
+    "revision": "9cdf99d357c65e644319",
+    "url": "/css/chunk-vendors.0d9171ac.css"
+  },
+  {
+    "revision": "13780e59a14f650aa4a3a9c7ed13efa3",
+    "url": "/img/1.13780e59.jpg"
+  },
+  {
+    "revision": "158c864beeab94b2ac3a60f408b17899",
+    "url": "/img/1.158c864b.jpg"
+  },
+  {
+    "revision": "116a15562eff06dbde7bb6103f6520a4",
+    "url": "/img/10.116a1556.jpg"
+  },
+  {
+    "revision": "523f9d2678a5c58eaadbd0146ece0afd",
+    "url": "/img/11.523f9d26.jpg"
+  },
+  {
+    "revision": "573b7db8dcc3781a48ebd77e8fb433bc",
+    "url": "/img/12.573b7db8.jpg"
+  },
+  {
+    "revision": "70d3ed6dbad36a4cc8be61c164bb826e",
+    "url": "/img/13.70d3ed6d.jpg"
+  },
+  {
+    "revision": "2beebb013fd4f2f8ae3ac9010da1aca5",
+    "url": "/img/14.2beebb01.jpg"
+  },
+  {
+    "revision": "30707770cd6bde381305865de44f515e",
+    "url": "/img/15.30707770.jpg"
+  },
+  {
+    "revision": "02c60fd1d4f0fb70cac89990d582e78d",
+    "url": "/img/16.02c60fd1.jpg"
+  },
+  {
+    "revision": "b92f0fb8becc08cf9933fe06c28cbb39",
+    "url": "/img/17.b92f0fb8.jpg"
+  },
+  {
+    "revision": "3071b9d7191fc6a39baab4f272213a4e",
+    "url": "/img/18.3071b9d7.jpg"
+  },
+  {
+    "revision": "961abe45412c2441a2b58638c0342700",
+    "url": "/img/19.961abe45.jpg"
+  },
+  {
+    "revision": "82e5226f1fbe1b63bb4b26275a47db0b",
+    "url": "/img/2.82e5226f.jpg"
+  },
+  {
+    "revision": "c3b66a1207fe7eb49c303194f51fa291",
+    "url": "/img/2.c3b66a12.jpg"
+  },
+  {
+    "revision": "6bede8d3da503f8ff2db255e11433dbe",
+    "url": "/img/20.6bede8d3.jpg"
+  },
+  {
+    "revision": "46b0d0b7a14b9e257611040840df9342",
+    "url": "/img/21.46b0d0b7.jpg"
+  },
+  {
+    "revision": "90ba82a3382256be3336382bac93d1e7",
+    "url": "/img/22.90ba82a3.jpg"
+  },
+  {
+    "revision": "a30acd8534e71cf563c3989ce3f4741d",
+    "url": "/img/23.a30acd85.jpg"
+  },
+  {
+    "revision": "5ad81624d037f85a26df71459fd9954e",
+    "url": "/img/24.5ad81624.jpg"
+  },
+  {
+    "revision": "86418dbdbd56cf347fd87c3d21bae061",
+    "url": "/img/25.86418dbd.jpg"
+  },
+  {
+    "revision": "403783b54e2783e76eab167ad7e6e7c2",
+    "url": "/img/26.403783b5.jpg"
+  },
+  {
+    "revision": "0629a308264155f7379defea6bddb713",
+    "url": "/img/27.0629a308.jpg"
+  },
+  {
+    "revision": "91d326de894f00ce925c4daaab2eebd7",
+    "url": "/img/28.91d326de.jpg"
+  },
+  {
+    "revision": "d18de73a49fe90359ecf1f45aad16bca",
+    "url": "/img/29.d18de73a.jpg"
+  },
+  {
+    "revision": "26343c5bb9a5a581c2ebaa3b5c92628f",
+    "url": "/img/2m.26343c5b.svg"
+  },
+  {
+    "revision": "2f94e3e3aa528a528cf39db565927e0c",
+    "url": "/img/3.2f94e3e3.jpg"
+  },
+  {
+    "revision": "d560a321c7a463f399a4da650711377d",
+    "url": "/img/3.d560a321.jpg"
+  },
+  {
+    "revision": "81f39dcfbd0bf07c8c5ac9964b7c2bb8",
+    "url": "/img/30.81f39dcf.jpg"
+  },
+  {
+    "revision": "b9b0dbd730913ae24b77a75e8a914312",
+    "url": "/img/31.b9b0dbd7.jpg"
+  },
+  {
+    "revision": "ca4d84d4d5e3b21b7ebfefab9be99548",
+    "url": "/img/32.ca4d84d4.jpg"
+  },
+  {
+    "revision": "b824f196e110a64c92377a78812cbaaf",
+    "url": "/img/33.b824f196.jpg"
+  },
+  {
+    "revision": "03bcf4013a1306ca3356820b37876d25",
+    "url": "/img/34.03bcf401.jpg"
+  },
+  {
+    "revision": "f0c0e21881ff8683ae5c3580ea0c6472",
+    "url": "/img/35.f0c0e218.jpg"
+  },
+  {
+    "revision": "bf5705051d9d03817cbf86f202082d47",
+    "url": "/img/36.bf570505.jpg"
+  },
+  {
+    "revision": "a2d3cdd0513f21c8730cf5f7eadda2a7",
+    "url": "/img/37.a2d3cdd0.jpg"
+  },
+  {
+    "revision": "6719a33f59284d81fab1e7686a4fc44e",
+    "url": "/img/38.6719a33f.jpg"
+  },
+  {
+    "revision": "ac8ab315b6974cc795ef593d881db632",
+    "url": "/img/39.ac8ab315.jpg"
+  },
+  {
+    "revision": "2eac4b1941d4b28c3f548e9e7bd8349e",
+    "url": "/img/4.2eac4b19.jpg"
+  },
+  {
+    "revision": "ff3c6170b58407ca6f1781e973b290b2",
+    "url": "/img/4.ff3c6170.jpg"
+  },
+  {
+    "revision": "0b30aaea27f8b797ef01341074ebc224",
+    "url": "/img/40.0b30aaea.jpg"
+  },
+  {
+    "revision": "3722ab403a46b1802151d0d559f9f60d",
+    "url": "/img/5.3722ab40.jpg"
+  },
+  {
+    "revision": "9119bd09d429e57aa81614b57f9f43df",
+    "url": "/img/5.9119bd09.jpg"
+  },
+  {
+    "revision": "312c74269a40ad1b14692bcd6fec35a3",
+    "url": "/img/6.312c7426.jpg"
+  },
+  {
+    "revision": "832ebfe68df9f1a512cf5862e11b6ef4",
+    "url": "/img/6.832ebfe6.jpg"
+  },
+  {
+    "revision": "345450c4dbf94c12eaee5b157399e0b2",
+    "url": "/img/7.345450c4.jpg"
+  },
+  {
+    "revision": "929664927c0c59f2b3cdfbf15c0bafe9",
+    "url": "/img/8.92966492.jpg"
+  },
+  {
+    "revision": "56cf9f713be75d18c761290dc0f1fc7c",
+    "url": "/img/9.56cf9f71.jpg"
+  },
+  {
+    "revision": "9fae5b2a089e33d395360708082c26c4",
+    "url": "/img/Lengyel_Intezet_logo.9fae5b2a.jpg"
+  },
+  {
+    "revision": "6496dc3233a668bc64ac052b3d40beec",
+    "url": "/img/amici.6496dc32.svg"
+  },
+  {
+    "revision": "fc2bf4e3f53712b3860a055d8684f5f9",
+    "url": "/img/balassi.fc2bf4e3.svg"
+  },
+  {
+    "revision": "3c3262e797ca8babba84f7875a950ca2",
+    "url": "/img/berki.3c3262e7.jpg"
+  },
+  {
+    "revision": "b59752543fd7f600dcda79821a8ae99b",
+    "url": "/img/berki.b5975254.jpg"
+  },
+  {
+    "revision": "da08e077435d424937ee4c60ec87c261",
+    "url": "/img/en.da08e077.svg"
+  },
+  {
+    "revision": "1d7b0d18019e4bac7f85265049027476",
+    "url": "/img/grain1.1d7b0d18.svg"
+  },
+  {
+    "revision": "b9b37ed39e8fc87822b275c18ec73d6b",
+    "url": "/img/grain2.b9b37ed3.svg"
+  },
+  {
+    "revision": "15ce2d6e4c6d88e7eb3709bd0d14ba62",
+    "url": "/img/hu.15ce2d6e.svg"
+  },
+  {
+    "revision": "2e0e3e500ac9a0f691900d3e27c6e8b2",
+    "url": "/img/hu.2e0e3e50.svg"
+  },
+  {
+    "revision": "3d57ed3fccc81326dc0bbb16f7d346da",
+    "url": "/img/kiskunhalas-wh.3d57ed3f.svg"
+  },
+  {
+    "revision": "25710734f86fb6ed44cbc3afe0d1329d",
+    "url": "/img/kiskunhalas.25710734.png"
+  },
+  {
+    "revision": "2a93cf2062ac01fd045825880c764e91",
+    "url": "/img/kiskunhalas.2a93cf20.svg"
+  },
+  {
+    "revision": "b608084431c8f96d0f05a8eef0537640",
+    "url": "/img/lemko.b6080844.svg"
+  },
+  {
+    "revision": "fdaf505a8eab2663245ca8a83b237aad",
+    "url": "/img/logomalopolska.fdaf505a.svg"
+  },
+  {
+    "revision": "1f6c208ad826394ed84475e9ea83e54a",
+    "url": "/img/mkidn-en.1f6c208a.svg"
+  },
+  {
+    "revision": "ef598ebdc7fe6e1e7709787154f0cb4d",
+    "url": "/img/mkidn-pl.ef598ebd.svg"
+  },
+  {
+    "revision": "23a42c94fe46cfe5e37f582f6bef15cc",
+    "url": "/img/museum.23a42c94.jpg"
+  },
+  {
+    "revision": "df5d3955791c48d1d0a416dd162e1030",
+    "url": "/img/museum.df5d3955.jpg"
+  },
+  {
+    "revision": "b7915fe177fc8f58c3b6f4b3554eaa5a",
+    "url": "/img/muzeum-white-horizontal.b7915fe1.svg"
+  },
+  {
+    "revision": "b7d98f82a3f986b54a46e5e418b9fd5a",
+    "url": "/img/muzeum.b7d98f82.svg"
+  },
+  {
+    "revision": "35602d9a183bc8154c2d9b525f43436e",
+    "url": "/img/nikifor.35602d9a.jpg"
+  },
+  {
+    "revision": "a5c615a3d405e967ed6d05bece1e8f98",
+    "url": "/img/nikifor.a5c615a3.jpg"
+  },
+  {
+    "revision": "fd6dde2db242360542a11fad6d067e16",
+    "url": "/img/ns.fd6dde2d.svg"
+  },
+  {
+    "revision": "66527952ccd87392b6d17e74deb7d4d4",
+    "url": "/img/pause.66527952.svg"
+  },
+  {
+    "revision": "7430dab4d71d8cf9c45d09dc3bfe3a69",
+    "url": "/img/pl.7430dab4.svg"
+  },
+  {
+    "revision": "9062cf48f345bff62b86caf14f47de86",
+    "url": "/img/pl.9062cf48.svg"
+  },
+  {
+    "revision": "6b4363129c5b48df1394dbdb4e6fded5",
+    "url": "/img/play.6b436312.svg"
+  },
+  {
+    "revision": "171f0f281b07c7a66e5d4ad1a576b056",
+    "url": "/img/profile.171f0f28.jpg"
+  },
+  {
+    "revision": "1cb40bf4948c20817f9c33f75576d672",
+    "url": "/img/profile.1cb40bf4.jpg"
+  },
+  {
+    "revision": "dbab45cd09aedb76b2b25ef7653c007c",
+    "url": "/img/projLogo.dbab45cd.svg"
+  },
+  {
+    "revision": "e9cd6519b6b85e8e8746fa066a31e425",
+    "url": "/img/project.e9cd6519.jpg"
+  },
+  {
+    "revision": "4ee29828b2696cd0ea1230f476f91f51",
+    "url": "/img/region.4ee29828.jpg"
+  },
+  {
+    "revision": "9de3e6e8382b6ba5b80baae55558f5b7",
+    "url": "/img/region.9de3e6e8.jpg"
+  },
+  {
+    "revision": "61565214d6c8c0a07b9cf0aa66eeb1ac",
+    "url": "/img/wystawa-1.61565214.jpg"
+  },
+  {
+    "revision": "ad85adb9699e424fc795b811d82922aa",
+    "url": "/img/wystawa-1.ad85adb9.jpg"
+  },
+  {
+    "revision": "60d297cd348094bfb6b2e10b6809bc90",
+    "url": "/img/wystawa-14.60d297cd.jpg"
+  },
+  {
+    "revision": "88ab2e57d7e418af1a97b8e97acda013",
+    "url": "/img/wystawa-15.88ab2e57.jpg"
+  },
+  {
+    "revision": "48fdcc9007efa719425b3a71555a098b",
+    "url": "/img/wystawa-17.48fdcc90.jpg"
+  },
+  {
+    "revision": "562e120f6139640058024ad3201d1f7b",
+    "url": "/img/wystawa-19.562e120f.jpg"
+  },
+  {
+    "revision": "969063a4a17aa5da0b7f771acf3d13a8",
+    "url": "/img/wystawa-2.969063a4.jpg"
+  },
+  {
+    "revision": "b617b0415275dce1b8711e34d011204e",
+    "url": "/img/wystawa-2.b617b041.jpg"
+  },
+  {
+    "revision": "69de63b6c38477bcbb16a9fb7f44fad5",
+    "url": "/img/wystawa-21.69de63b6.jpg"
+  },
+  {
+    "revision": "3b2cf29403d2c757398048c55a05905f",
+    "url": "/img/wystawa-23.3b2cf294.jpg"
+  },
+  {
+    "revision": "e49e2c3b03c856d94ace8420247e66ee",
+    "url": "/img/wystawa-24.e49e2c3b.jpg"
+  },
+  {
+    "revision": "901661ede637831679ae7d7b477415c6",
+    "url": "/img/wystawa-27.901661ed.jpg"
+  },
+  {
+    "revision": "f96b43ae5669ae96ce236ee2d31c6c44",
+    "url": "/img/wystawa-3.f96b43ae.jpg"
+  },
+  {
+    "revision": "12038369fab5e4ad266081f84b48ea94",
+    "url": "/img/wystawa-4.12038369.jpg"
+  },
+  {
+    "revision": "cfac0175eeebbf777ba231369975d503",
+    "url": "/img/wystawa-4.cfac0175.jpg"
+  },
+  {
+    "revision": "45cac18cb198468c8c107584ee3ce89c",
+    "url": "/img/wystawa-5.45cac18c.jpg"
+  },
+  {
+    "revision": "d66a3dfe19cd682e187705734d957381",
+    "url": "/img/wystawa-6.d66a3dfe.jpg"
+  },
+  {
+    "revision": "5d706c86f162a8899287bc90fd298db9",
+    "url": "/img/wystawa-7.5d706c86.jpg"
+  },
+  {
+    "revision": "b52629246275884a43a9fe68cdf15e22",
+    "url": "/img/wystawa-8.b5262924.jpg"
+  },
+  {
+    "revision": "9c554c67260add2a98e0180c3e960d20",
+    "url": "/img/wystawa-9.9c554c67.jpg"
+  },
+  {
+    "revision": "cdf2d672ed2abe4bc24e4aa20b6a2888",
+    "url": "/img/zoom.cdf2d672.svg"
+  },
+  {
+    "revision": "5be831db54ae5b8e77c06ff5003a7287",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4f5881ebfc26d2731e2",
+    "url": "/js/app.853d28b6.js"
+  },
+  {
+    "revision": "9cdf99d357c65e644319",
+    "url": "/js/chunk-vendors.729ef715.js"
+  },
+  {
+    "revision": "88ae368c341cf31bc8d369906921aa24",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
